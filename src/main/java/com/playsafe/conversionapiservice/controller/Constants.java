7795c844b21cc87f4acb0bcda7e1fd0858d81234
Sherperd Nkopodi Maepa <maepans@gmail.com>
2020-08-06 18:56:43 +0200

    Modified controller added constants values

---
+++ src/main/java/com/playsafe/conversionapiservice/controller/Constants.java
@@ -0,0 +1,7 @@
+package com.playsafe.conversionapiservice.controller;
+
+final class Constants {
+    public static final double TEMP = 273.15;
+    public static final double MILES = 1.60934;
+    public static final double KILOMETERS = 0.6213715;
+}
